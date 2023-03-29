3d49d904bacd800348c9dc4d79f343962e4feae3
artem <bakirovartem69@gmail.com>
2023-03-29 02:47:15 +0200

    svelte projekt

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import Time from "./lib/Time.svelte";
+  import ayanami from "./assets/Rei.webp"
+</script>
+
+<main>
+  <img src={ayanami} alt="I rei Ayanami">
+  <div class="card">
+    Ты тратишь своё время, изучая ненужные ненужные технологии...
+    <br/>
+    <br/>
+    <p>
+      Уже прошло <br/> <br/><Time/>
+    </p>
+    <br/>
+    <br/>
+    <Counter />
+  </div>
+</main>
+
+<style>
+  main {
+    background-color: #c4c4e2;
+  }
+
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button class="button" on:click={increment}>
+  ПОН
+</button>
+
+<style>
+  .button{
+    border: 3px solid blue;
+  }
+
+  .button:hover{
+    border: 3px solid #0b6e0b;
+  }
+</style>
